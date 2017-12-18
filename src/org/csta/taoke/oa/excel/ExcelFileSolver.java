027169be94a715a83b38924da6f37bd4ff7d3613
xiaolulwr <xiaolulwr@outlook.com>
2017-12-18 14:57:23 +0800

    Import and Export Excel File

--- src/org/csta/taoke/oa/excel/ExcelFileSolver.java
+++ src/org/csta/taoke/oa/excel/ExcelFileSolver.java
@@ -1,12 +1,21 @@
 package org.csta.taoke.oa.excel;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.Vector;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
@@ -14,8 +23,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * 导入导出Excel文件
  * 
  * 修订版本：
- * 2017-12-17 首次编写
- * 2017-12-18 00:25 写不动了......睡觉
+ * 2017-12-18 首次编写
  * 
  * @author 路伟饶
  *
@@ -27,27 +35,125 @@ public class ExcelFileSolver {
 		this.targetFile = file;
 	}
 	
-	public void writeData(Vector<Vector<String>> data) {
-		XSSFWorkbook xlsx = new XSSFWorkbook();
-	}
-	public Vector<Vector<String>> readData() throws IllegalArgumentException, InvalidFormatException, IOException {
+	public void writeData(Vector<Vector<String>> data) throws InvalidFormatException, IOException {
 		if (targetFile.getName().endsWith(".xlsx")) {
-			XSSFWorkbook xlsx = new XSSFWorkbook(targetFile);
+			XSSFWorkbook xlsx = new XSSFWorkbook();
+			XSSFSheet sheet = xlsx.createSheet();
+			int i = 0;
+			for (Iterator<Vector<String>> rowIterator = data.iterator(); rowIterator.hasNext();++i) {
+				Vector<String> vector = (Vector<String>) rowIterator.next();
+				XSSFRow row = sheet.createRow(i);
+				int j = 0;
+				for (Iterator<String> colIterator = vector.iterator(); colIterator.hasNext();++j) {
+					String string = (String) colIterator.next();
+					XSSFCell cell = row.createCell(j);
+					cell.setCellValue(string);
+				}
+			}
+			FileOutputStream fos = new FileOutputStream(targetFile);
+			xlsx.write(fos);
+			xlsx.close();
 		}
 		else if (targetFile.getName().endsWith(".xls")) {
 			HSSFWorkbook xls = new HSSFWorkbook();
-		
+			HSSFSheet sheet = xls.createSheet();
+			int i = 0;
+			for ( Iterator<Vector<String>> rowIterator = data.iterator(); rowIterator.hasNext();++i) {
+				Vector<String> vector = (Vector<String>) rowIterator.next();
+				HSSFRow row = sheet.createRow(i);
+				int j = 0;
+				for (Iterator<String> colIterator = vector.iterator(); colIterator.hasNext();++j) {
+					String string = (String) colIterator.next();
+					HSSFCell cell = row.createCell(j);
+					cell.setCellValue(string);
+				}
+			}
+			FileOutputStream fos = new FileOutputStream(targetFile);
+			xls.write(fos);
+			xls.close();
 		}
 		else {
 			throw new IllegalArgumentException("不是Excel文件后缀");
 		}
-		return null;
+	}
+	public Vector<Vector<String>> readData() throws IllegalArgumentException, InvalidFormatException, IOException {
+		if (targetFile.getName().endsWith(".xlsx")) {
+			Vector<Vector<String>> data = new Vector<Vector<String>>();
+			XSSFWorkbook xlsx = new XSSFWorkbook(targetFile);
+			XSSFSheet sheet = xlsx.getSheetAt(0);
+			for (int i = 0; i <= sheet.getLastRowNum(); ++i) {
+				if (i < sheet.getFirstRowNum()) {
+					data.add(null);
+					continue;
+				}
+				XSSFRow row = sheet.getRow(i);
+				Vector<String> rowData = new Vector<String>();
+				for (int j = 0; j <= row.getLastCellNum(); ++j) {
+					if (j < row.getFirstCellNum()) {
+						rowData.add(null);
+						continue;
+					}
+					XSSFCell cell = row.getCell(j);
+					if (cell.getCellTypeEnum().equals(CellType.STRING)) {
+						rowData.add(cell.getStringCellValue());
+					}
+					else if (cell.getCellTypeEnum().equals(CellType.NUMERIC)) {
+						rowData.add(Double.toString(cell.getNumericCellValue()));
+					}
+					else {
+						rowData.clear();
+						data.clear();
+						xlsx.close();
+						throw new InvalidFormatException("");
+					}
+				}
+				data.add(rowData);
+			}
+			xlsx.close();
+			return data;
+		}
+		else if (targetFile.getName().endsWith(".xls")) {
+			Vector<Vector<String>> data = new Vector<Vector<String>>();
+			POIFSFileSystem fileSystem = new POIFSFileSystem(targetFile, true);
+			HSSFWorkbook xls = new HSSFWorkbook(fileSystem);
+			HSSFSheet sheet = xls.getSheetAt(0);
+			for (int i = 0; i <= sheet.getLastRowNum(); ++i) {
+				if (i < sheet.getFirstRowNum()) {
+					data.add(null);
+					continue;
+				}
+				HSSFRow row = sheet.getRow(i);
+				Vector<String> rowData = new Vector<String>();
+				for (int j = 0; j <= row.getLastCellNum(); ++j) {
+					if (j < row.getFirstCellNum()) {
+						rowData.add(null);
+						continue;
+					}
+					HSSFCell cell = row.getCell(j);
+					if (cell.getCellTypeEnum().equals(CellType.STRING)) {
+						rowData.add(cell.getStringCellValue());
+					}
+					else if (cell.getCellTypeEnum().equals(CellType.NUMERIC)) {
+						rowData.add(Double.toString(cell.getNumericCellValue()));
+					}
+					else {
+						rowData.clear();
+						data.clear();
+						xls.close();
+						throw new InvalidFormatException("");
+					}
+				}
+				data.add(rowData);
+			}
+			xls.close();
+			return data;
+		}
+		else {
+			throw new IllegalArgumentException("不是Excel文件后缀");
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("Testing Import and Export Excel File ");
 	}
 }
-
-
-
-
-
-
 
